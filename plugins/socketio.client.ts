de6797c3a1b8f40a171143cd9b909e1a8d297290
Bankole Esan <techybanky@gmail.com>
2024-09-13 13:25:59 +0100

    fix: runtime config socket io error

--- plugins/socketio.client.ts
+++ plugins/socketio.client.ts
@@ -1,12 +1,12 @@
 import { io } from "socket.io-client";
 
 //Socket Client
-const config = useRuntimeConfig();
-const apiBaseUrl = config.public.API_BASE_URL;
-const filesSocket = io(`${apiBaseUrl}`, {
-  path: '/api/v1/files/socket'
-});
 export default defineNuxtPlugin(() => {
+  const config = useRuntimeConfig();
+  const apiBaseUrl = config.public.API_BASE_URL;
+  const filesSocket = io(`${apiBaseUrl}`, {
+    path: '/api/v1/files/socket'
+  });
   return {
     provide: {
       fileio: filesSocket,
